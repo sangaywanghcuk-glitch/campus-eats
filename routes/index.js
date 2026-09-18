73072a54930516e2f3e74c9d0f2935c1e0523820
sangaywangchuk <sangaywanghcuk@gmail.com>
2026-09-18 12:29:28 +0600

    Unit IV Lab 4: Order model, full CRUD, validation, aggregation, and transactions

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,14 @@ const menuController = require('../controllers/menuController');
 
 router.get('/restaurants/:id/menu', menuController.getMenuByRestaurant); 
 
-const orderController = require('../controllers/orderController');
 
-router.post('/orders', orderController.createOrder);
+
+
+const orderController = require('../controllers/orderController'); 
+ 
+router.post('/orders', orderController.createOrder); 
+router.get('/orders/:id', orderController.getOrder);
+
+router.post('/orders/:id/update', orderController.updateOrder); 
+
+router.post('/orders/:id/cancel', orderController.cancelOrder); 
